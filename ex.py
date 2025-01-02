d59391c78f625f114238612a6d59f0aa09996ffd
Vlad Novice <hakettip@mail.ru>
2025-01-02 18:50:42 +0500

    asd

---
+++ ex.py
@@ -0,0 +1,14 @@
+import sqlite3 as sq
+
+with sq.connect("saper.db") as con:
+    cur = con.cursor()
+
+
+    
+    cur.execute("""CREATE TABLE users (
+    name TEXT NOT NULL,
+    sex INTEGER DEFAULT 1,
+    old INTEGER,
+    score INTEGER
+    )""")
+
